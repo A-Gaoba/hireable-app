208a48579cf50a5ac1c31f8bab5daae26e8b78a0
a-gaoba <abdulrahman1997abdo@gmail.com>
2024-12-04 20:50:26 +0300

    first comment

---
+++ src/components/Hero.tsx
@@ -0,0 +1,65 @@
+"use client"
+import Link from 'next/link'
+import Navbar from './Navbar'
+
+function HeroSection() {
+  return (
+    <>
+      <Navbar />
+      <section className="min-h-max bg-white dark:bg-gray-950">
+        <div className="absolute top-0 inset-x-0 h-64 flex items-start">
+          <div className="h-24 w-2/3 bg-gradient-to-br from-purple-500 opacity-20 blur-2xl dark:from-purple-700 dark:invisible dark:opacity-40">
+          </div>
+          <div className="h-20 w-3/5 bg-gradient-to-r from-blue-600 opacity-40 blur-2xl dark:from-purple-700 dark:opacity-40">
+          </div>
+        </div>
+        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-2/5 aspect-[2/0.5] bg-gradient-to-br from-purple-600 to-violet-400 rounded-full opacity-50 blur-2xl">
+        </div>
+        <div className="relative mx-auto pt-32 pb-24 lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 text-center space-y-10">
+          <h1 className="text-gray-900 dark:text-white mx-auto max-w-5xl font-bold text-4xl/tight sm:text-5xl/tight lg:text-6xl/tight xl:text-7xl/tight">
+            HireableJS
+          </h1>
+          <h1 className="text-gray-900 dark:text-white mx-auto max-w-5xl font-bold text-4xl/tight sm:text-5xl/tight lg:text-6xl/tight xl:text-7xl/tight">
+            The zone for all desperate JavaScript developers
+          </h1>
+          <p className="text-gray-700 dark:text-gray-300 mx-auto max-w-2xl">
+            Whether you’re debugging nightmares or searching for the perfect function, we’re here to provide modern tools, solutions,
+            and opportunities to elevate your JavaScript journey.          </p>
+          <div className="flex justify-center items-center flex-wrap mx-auto gap-4">
+            <Link href="#" className="flex items-center h-12 px-6 rounded-full bg-purple-600 text-white border border-purple-600">
+              Hire us Now
+            </Link>
+            <Link href="#" className="flex items-center h-12 px-6 rounded-full bg-gray-100 dark:bg-gray-900 text-purple-700 dark:text-gray-300 border border-gray-200 dark:border-gray-800">
+              Learn more
+            </Link>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-8 w-full">
+              {/* Card 1 */}
+              <div className="bg-white dark:bg-gray-950 shadow-lg shadow-gray-200/50 dark:shadow-transparent border border-gray-100/80 dark:border-gray-900/80 p-6 rounded-lg flex flex-col justify-center items-center text-center text-gray-700 dark:text-gray-300">
+                <span className="font-semibold text-2xl sm:text-3xl lg:text-4xl text-gray-900 dark:text-white">
+                  2 Years
+                </span>
+                <span className="text-sm sm:text-base">
+                  Experiences
+                </span>
+              </div>
+
+              {/* Card 2 */}
+              <div className="bg-white dark:bg-gray-950 shadow-lg shadow-gray-200/50 dark:shadow-transparent border border-gray-100/80 dark:border-gray-900/80 p-6 rounded-lg flex flex-col justify-center items-center text-center text-gray-700 dark:text-gray-300">
+                <span className="font-semibold text-2xl sm:text-3xl lg:text-4xl text-gray-900 dark:text-white">
+                  4k
+                </span>
+                <span className="text-sm sm:text-base">
+                  Projects
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { ModeToggle } from "./ModToggle";
+
+function Navbar() {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenNavbar((prevState) => !prevState);
+  };
+
+  return (
+    <header className="absolute left-0 top-0 w-full flex items-center h-24 z-40">
+      <nav className="relative mx-auto lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 flex gap-x-5 justify-between items-center">
+        {/* Logo Section */}
+        <div className="flex items-center min-w-max">
+          <Link href="#" className="font-semibold flex items-center gap-x-2">
+            <div className="flex items-center -space-x-3">
+              <span className="h-6 aspect-square bg-purple-600 dark:bg-purple-500 rounded-full flex" />
+              <span className="h-6 aspect-square bg-violet-600 dark:bg-violet-400 blur rounded-full flex" />
+            </div>
+            <span className="text-lg text-gray-700 dark:text-gray-300">HirabalJS</span>
+          </Link>
+        </div>
+
+        {/* Navbar Links */}
+        <div
+          className={`absolute top-full left-0 bg-white dark:bg-gray-950 lg:bg-transparent border-b border-gray-200 dark:border-gray-800 py-8 lg:py-0 px-5 sm:px-10 md:px-12 lg:px-0 lg:border-none lg:w-max lg:space-x-16 lg:top-0 lg:relative lg:flex duration-300 lg:transition-none ease-linear ${openNavbar
+            ? "translate-y-0 opacity-100 visible"
+            : "translate-y-10 opacity-0 invisible lg:visible lg:translate-y-0 lg:opacity-100"
+            }`}
+        >
+          <ul className="flex flex-col lg:flex-row gap-6 lg:items-center text-gray-700 dark:text-gray-300 lg:w-full lg:justify-center">
+            <li>
+              <Link href="#" className="px-2 transition-colors py-2.5 hover:text-purple-600">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="px-2 transition-colors py-2.5 hover:text-purple-600">
+                Company
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="px-2 transition-colors py-2.5 hover:text-purple-600">
+                prices
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="px-2 transition-colors py-2.5 hover:text-purple-600">
+                contact
+              </Link>
+            </li>
+          </ul>
+          <div className="flex flex-col sm:flex-row sm:items-center gap-4 lg:min-w-max mt-10 lg:mt-0">
+
+            <ModeToggle />
+            <Link
+              href="#"
+              className="flex items-center justify-center w-full sm:w-auto h-12 px-6 rounded-full bg-gray-100 dark:bg-gray-900 text-purple-600 dark:text-gray-300 border border-gray-200 dark:border-gray-800"
+            >
+              Get In Touch
+            </Link>
+          </div>
+        </div>
+
+        {/* Hamburger Menu */}
+        <div className="flex items-center lg:hidden">
+          <button
+            onClick={toggleNavbar}
+            aria-label="Toggle navbar"
+            className="outline-none border-l border-l-gray-100 dark:border-l-gray-800 pl-3 relative py-3 flex flex-col justify-center items-center"
+          >
+            <span
+              aria-hidden="true"
+              className={`h-0.5 w-6 rounded bg-gray-800 dark:bg-gray-300 transition duration-300 ${openNavbar ? "rotate-45 translate-y-[0.33rem]" : ""
+                }`}
+            />
+            <span
+              aria-hidden="true"
+              className={`mt-1.5 h-0.5 w-6 rounded bg-gray-800 dark:bg-gray-300 transition duration-300 ${openNavbar ? "-rotate-45 -translate-y-[0.33rem]" : ""
+                }`}
+            />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
